432662b47987886fe66fa284cdbda9998e0a89cb
Guozhang Wang <guwang@linkedin.com>
2014-02-20 17:44:41 -0800

    kafka-1235; Enable server to indefinitely retry on controlled shutdown; patched by Guozhang Wang; reviewed by Jun Rao

--- core/src/main/scala/kafka/consumer/SimpleConsumer.scala
+++ core/src/main/scala/kafka/consumer/SimpleConsumer.scala
@@ -46,10 +46,8 @@ class SimpleConsumer(val host: String,
   }
 
   private def disconnect() = {
-    if(blockingChannel.isConnected) {
-      debug("Disconnecting from " + host + ":" + port)
-      blockingChannel.disconnect()
-    }
+    debug("Disconnecting from " + host + ":" + port)
+    blockingChannel.disconnect()
   }
 
   private def reconnect() {
@@ -67,9 +65,9 @@ class SimpleConsumer(val host: String,
   
   private def sendRequest(request: RequestOrResponse): Receive = {
     lock synchronized {
-      getOrMakeConnection()
       var response: Receive = null
       try {
+        getOrMakeConnection()
         blockingChannel.send(request)
         response = blockingChannel.receive()
       } catch {
